cbc516e39770f8edd8f25bde00e5b371fe726728
Jorge Garcia <70530205+Bluexfusion@users.noreply.github.com>
2020-09-21 11:10:50 -0400

    initial change

---
+++ script.js
@@ -0,0 +1,10 @@
+var wrapperEle = document.body.querySelector(".wrapper");
+var prompter = Number(prompt("Type a number"));
+var adding = (number+prompter);
+
+if(!isNaN(123))
+var number = 5;
+
+
+
+document.body.querySelector(".number").innerHTML="Program is over"
